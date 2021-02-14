8dca93712804f531aa6f85e5baa0103882a1a4c7
achugo <emeka@brandmobileafrica.com>
2021-02-14 21:41:02 +0100

    commit

--- src/components/VideoPlayer/index.js
+++ src/components/VideoPlayer/index.js
@@ -3,6 +3,19 @@ import video1 from "../../video_assets/dummy.mp4";
 import poster from "../../img-assets/video_background.svg";
 import VideoBackground from "../StyledComponents/VideoBackground";
 import "./styles.css";
+import play from "../../img-assets/play.png";
+import styled from "styled-components";
+
+const Center = styled.div`
+  display: flex;
+  height: 45vh;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    max-width: 100px;
+  }
+`;
 
 const VideoPlayer = ({ video = video1, videoPoster = poster }) => {
   return (
@@ -12,9 +25,10 @@ const VideoPlayer = ({ video = video1, videoPoster = poster }) => {
         <br /> baggage off <br />
         you
       </h4>
-      <div>
-        <i className="fa fa-play-circle-o play" aria-hidden="true"></i>
-      </div>
+      <Center>
+        <img src={play} alt="play button" />
+        {/* <i className="fa fa-play-circle-o play" aria-hidden="true"></i> */}
+      </Center>
     </VideoBackground>
   );
 };
